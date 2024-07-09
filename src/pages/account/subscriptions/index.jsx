a3e1d1fae84dc844beadf75f0abec18d052bc1ab
Klaidi Dingu <klaididingu@gmail.com>
2024-07-09 15:21:44 +0200

    add work in progress for some pages

--- src/pages/account/subscriptions/index.jsx
+++ src/pages/account/subscriptions/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import MainLayout from "@/components/main-layout";
 import Container from "@/components/container";
+import WorkInProgress from "@/components/work-in-progress";
 
 function Subscriptions() {
     return (
         <MainLayout>
-            <Container className="my-10">Subscriptions</Container>
+            <Container className="my-10">
+                <WorkInProgress />
+            </Container>
         </MainLayout>
     );
 }
